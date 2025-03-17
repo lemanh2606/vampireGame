82fc7e65d620a5feefcc3df8f79882ad19e5d127
tranhls <anhdnthe171790@fpt.edu.vn>
2025-03-17 07:53:59 +0700

    update menu select charactor

---
+++ Vampire Game/Assets/Script/ButtonClickEffect.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ButtonClickEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Vector3 originalScale;
+    public float hoverScaleFactor = 1.2f; // Kích thước khi di chuột vào
+    public float duration = 0.08f; // Thời gian hiệu ứng nhanh hơn
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScaleEffect(originalScale * hoverScaleFactor));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScaleEffect(originalScale));
+    }
+
+    IEnumerator ScaleEffect(Vector3 targetScale)
+    {
+        float time = 0;
+        Vector3 startScale = transform.localScale;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float progress = time / duration;
+            progress = Mathf.Sin(progress * Mathf.PI * 0.5f); // Ease Out: Nhanh lúc đầu, chậm về cuối
+            transform.localScale = Vector3.Lerp(startScale, targetScale, progress);
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
+    }
+}
